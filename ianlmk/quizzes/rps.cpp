aa7ae74a4634d16d9daa9b0c9e02a0f5191ac11f
player2 <ian.lamottekerr@gmail.com>
2022-02-19 09:35:47 -0800

    updated output

--- ianlmk/quizzes/rps.cpp
+++ ianlmk/quizzes/rps.cpp
@@ -16,23 +16,29 @@
 using namespace std;
 
 int main() {
+  // seed rand
   srand(time(0));
+  // instantiate variables
   string name = "";
   char playerHand;
   int systemHand;
   int i = 0;
-
-
+  // clear screen
   cout << "\033[2J\033[1;1H";
-
-  cout << "Let's play Rock(r), Paper(p), Scissors(s)" << endl;
+  // welcome message
+  cout << "#############################################\n"
+       << "# Let's play Rock(r), Paper(p), Scissors(s) #\n" 
+       << "#############################################\n" << endl;
   cout << "Name please: " << endl;
+  // get name, spaces allowed
   getline(cin, name);
   
+  // setup input check loop marker
   top:
-    cout << "What's your play " << name << "\nRock(r), Paper(p), or Scissors(s): ";
+    cout << "What's your play " << name << "?\n    (R)ock\n    (P)aper\n    (S)cissors\n\n    : ";
     cin >> playerHand;
-    
+    cout << "_________________" << endl;
+    // conditions for failure, give the user 1 chance to correct, then exit on failure 2. 
     if ((playerHand != 'r') && (playerHand != 'R') && (playerHand != 'p') && (playerHand != 'P') && (playerHand != 's') && (playerHand != 'S')) {
       if (i < 3) {
         i++;
@@ -44,52 +50,54 @@ int main() {
         goto top;
       }
     }
-    
+  // assign systemHand to a random number between 0 and 3  
   systemHand = rand() % 3;
   // r = 0
   // p = 1
   // s = 2
 
+  // Compare playerHand and systemHand, then output the result messages. 
   switch (playerHand) {
     case 'r': case 'R':
       if (systemHand == 0) {
-        cout << "Computer plays R(ock)\n"         
+        cout << "Computer plays:\n    (R)ock\n"
              << "\nIt's a TIE!\n" << endl;
       } else if (systemHand == 1) {
-        cout << "Computer plays P(aper)\n"
+        cout << "Computer plays:\n    (P)aper\n"
              << "\nThe Computer won!\n" << endl;
       } else if (systemHand == 2) {
-        cout << "\nComputer plays S(cissors)\n"
+        cout << "\nComputer plays:\n    (S)cissors)\n"
              << name << " Wins!\n" << endl;
       }
       break;
     case 'p': case 'P':
       if (systemHand == 1) {
-        cout << "Computer plays P(aper)\n"
+        cout << "Computer plays:\n    (P)aper\n"
              << "\nIt's a TIE!\n" << endl;
       } else if (systemHand == 2) {
-        cout << "Computer plays S(cissors)\n"
+        cout << "Computer plays:\n    (S)cissors\n"
              << "\nThe Computer won!\n" << endl;
       } else if (systemHand == 0) {
-        cout << "\nComputer plays R(ock)\n"
+        cout << "\nComputer plays:\n    (R)ock\n"
              << name << " Wins!\n" << endl;
       }
       break;
     case 's': case 'S':
       if (systemHand == 2) {
-        cout << "Computer plays S(cissors)\n"
+        cout << "Computer plays:\n    (S)cissors\n"
              << "\nIt's a TIE!\n" << endl;
       } else if (systemHand == 0) {
-        cout << "Computer plays R(ock)\n"
+        cout << "Computer plays:\n    (R)ock\n"
              << "\nThe Computer won!\n" << endl;
       } else if (systemHand == 1) {
-        cout << "\nComputer plays P(aper)\n"
+        cout << "\nComputer plays:\n    (P)aper\n\n"
              << name << " Wins!\n" << endl;
       }
       break;
     default:
       break;
   }
+  // goodbye message
   cout << "Thanks for playing!" << endl;
 
   return 0;
