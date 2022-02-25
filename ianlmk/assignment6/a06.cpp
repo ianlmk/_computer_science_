e645778360dce206b7f4d7b4b61ac7889c4a1487
ianlmk <ian.lamottekerr@gmail.com>
2022-02-24 19:34:58 -0800

    just making a copy

--- ianlmk/assignment6/a06.cpp
+++ ianlmk/assignment6/a06.cpp
@@ -23,33 +23,66 @@ int main() {
   const double LOAD_FACTOR = 0.64;
   const double PI = M_PI;
 
-  double gumballRadius;
-  double containerSize;
-  double gumballVolume;
-  int gumballCount;
-  char response;
+  double gumballRadius = 0.0;
+  double containerSize = 0.0;
+  double gumballVolume = 0.0;
+  double totalGumballs = 0.0;
+  double gumballAverage = 0.0;
+  double largestGumball = 0.0;
+  double largestGumballJar = 0.0;
+  int gumballCount = 0;
+  int iterCount = 0;
+  char selection ='y';
 
   // Clear the screen
   cout << "\033[2J\033[1;1H";
   // Program header
-  cout << "Welcome to my gumball guesser program!" << endl;
-  // loop setup
-  top:  
-    cout << "Enter the radius of a gumball (cm) and the volume of a jar (mL) separated by a space: ";
-    cin >> gumballRadius >> containerSize;
+  cout << "##########################################################\n"
+       << "#         Welcome to my gumball guesser program!         #\n" 
+       << "##########################################################" 
+       << endl;
+
+  // loop setup on a 'y' selection
+  while (selection != 'n') { 
+    iterCount++;
+    cout << "\nEnter the radius of a gumball (cm): "; 
+    cin >> gumballRadius;
+    cout << "Enter the volume of the jar (mL) holding the gumballs: ";
+    cin >> containerSize;
+  
+    // volume stuff and jar size stuff
     gumballVolume = (4.0 / 3.0) * M_PI * pow(gumballRadius, 3.0);
+    if (largestGumball < gumballVolume) {
+      largestGumball = gumballVolume;
+      largestGumballJar = containerSize;
+    }  
+
+    //gumball count, total, and average over all iterations
     gumballCount = (containerSize * LOAD_FACTOR ) / gumballVolume;
+    totalGumballs = totalGumballs + gumballCount;
+    gumballAverage = totalGumballs / iterCount;  
 
-    cout << "Estimate of gumballs in the jar: " << fixed << setprecision(2) << gumballCount << endl;
+    // iteration output
+    cout << "\nEstimated number of gumballs in the jar: " << fixed << setprecision(2) << gumballCount << endl;
+    cout << "\n=========================================\n";
+    // update selection
+    cin.ignore();
     cout << "\nDo you want to enter more (y/n): ";
+    selection = cin.get();
+    cout << "\n=========================================";
+    // Invalid input condition    
+    if ((selection != 'y') && (selection !='n')) {
+      cout << "\nInvalid selection. Thank you for using my program!\n" << endl;
+      exit(0);
+    }
+  }
 
-    cin >> response;
-      if (response == 'y') {
-        goto top;
-      } else {
-        cout << "Thank you for using my program!\n" << endl;
-        exit(0);
-      }
+    cout << setw(50) << left << "\nNumber of plays: " << right << iterCount << endl;
+    cout << setw(50) << left << "\nAverage gumball count: " << right << gumballAverage;
+    cout << setw(50) << left << "\nLargest gumball: " << right << largestGumball;
+    cout << setw(50) << left << "\nJar size holding the largest gumballs: " << right << largestGumballJar;
+    cout << "\n_________________________________________________________\n"; 
+    cout << "\nThank you for using my program!" << endl;; 
 
   return 0;
 }
