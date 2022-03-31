4a1d7429696150d48056fc68508c665c062fadcd
ianlmk <ian.lamottekerr@gmail.com>
2022-03-31 12:15:06 -0700

    laying out the class assignments

---
+++ ianlmk/cs161b/assignment5/a05.cpp
@@ -0,0 +1,24 @@
+/*
+###################################################################################
+# Author:           Ian LaMotte-Kerr
+# Assignment:       Assignment 5
+# Date:             $(date -j -f "%a %b %d %T %Z %Y" "`date`" "+%s")
+# Description:
+# Input:            
+# Output:          
+# Sources:          
+###################################################################################
+*/
+
+#include <iostream>
+#include <iomanip>
+#include <string>
+#include <cmath>
+
+//using namespace std;
+
+int main () {
+
+
+  return 0;
+}
