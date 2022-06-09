ef8a71a6fb6a3ba752922b6e6104c13fa018b19b
ianlmk <ian.lamottekerr@gmail.com>
2022-06-08 21:31:04 -0700

    assignment 7

--- ianlmk/cs161b/assignment7/Occupation.h
+++ ianlmk/cs161b/assignment7/Occupation.h
@@ -1,20 +1,32 @@
-//a07.h
-//struct file for item type
+//# Author:           Ian LaMotte-Kerr
+//# Assignment:       Assignment 7 header file
 #pragma once
-#include <fstream>
+
 #include <iostream>
+#include <fstream>
 #include <cstring>
 #include <iomanip>
 
-const int MAXCHAR = 25;
-
 using namespace std;
 
-struct item {
-  char occupation[MAXCHAR];
-  int employed;
-  int automation;
-};
+//constant for number of items
+const int ITEMS = 25;
+//constant for number of characters
+const int MAXCHAR = 20;
+
+// struct
+// format: 
+ struct item {
+     char occupation[MAXCHAR];
+     int employed;
+     int automation;
+   };
 
 
+//function prototypes
+bool openFile(ifstream &inFile);
+int loadData(ifstream &inFile);
+void printData(int i);
+void findLow(int rows, char occupationl[], double &lowPercent);
+void findHigh(int rows, char occupationh[], double &highPercent);
 
--- ianlmk/cs161b/assignment7/a07.cpp
+++ ianlmk/cs161b/assignment7/a07.cpp
@@ -1,7 +1,7 @@
 /*
 ###################################################################################
 # Author:           Ian LaMotte-Kerr
-# Assignment:       Assignment 6
+# Assignment:       Assignment 7
 # Date:             $(date -j -f "%a %b %d %T %Z %Y" "`date`" "+%s")
 # Description:
 # Input:            aidata.txt 
@@ -10,65 +10,35 @@
 ###################################################################################
 */
 
-#include <iostream>
-#include <fstream>
-#include <cstring>
-#include <iomanip>
+#include "Occupation.h"
 
-using namespace std;
-
-//constant for number of items
-const int ITEMS = 25;
-//constant for number of characters
-const int MAXCHAR = 20;
-
-// struct
-// format: 
-/* struct item {
-     char occupation[MAXCHAR];
-     int employed;
-     int automation;
-   };
-*/
-item jobs[ITEMS];
-
-//function prototypes
-bool openFile(ifstream &inFile);
-int loadData(ifstream &inFile, item jobs[]);
-void printData(item jobs[], int count);
-void findHigh(item jobs[], char occupationh[], double &highPercent);
-void findLow(item jobs[], char occupationl[], double &lowPercent);
-
-//main function that will call openFile, loadData and calcCalories
 int main() {
 
   ifstream inFile;
-  int i = 0;
   double highPercent = 0.0;  
   double lowPercent = 0.0;
-  char occupationh[MAXCHAR];  
-  char occupationl[MAXCHAR];  
+  int i = 0;
+  char occupationl[MAXCHAR];
+  char occupationh[MAXCHAR];
 
-  cout << "\033[2J\033[1;1H";
+
   if(!openFile(inFile)) {
     cout << "File did not open! Program terminating!!" << endl;
     exit(1);
+  } else {
+    cout<<"File opened."<<endl; 
   }
 
-  // init data from file
-  i = loadData(inFile,jobs);
+  i = loadData(inFile);
 
-  // format and output the data from f(x): printData
-  printData(jobs, i);
-
-
-  findHigh(jobs,occupationh,highPercent);
+  printData(i);
+  
+  findHigh(i,occupationh,highPercent);
   cout<<occupationh<<" has the highest share at: ("<<setprecision(4)<<highPercent<<"%)"<<endl;
-
-
-  findLow(jobs,occupationl,lowPercent);
+  findLow(i,occupationl,lowPercent);
   cout<<occupationl<<" has the lowest share at: ("<<setprecision(4)<<lowPercent<<"%)"<<endl;
 
   cout << "Thank you!!" << endl;
   return 0;
 }
+
